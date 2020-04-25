fc35c441192dd63a16e5f67974326a441fcb526a
goeunk <goeunkwak22@gmail.com>
2020-04-25 04:07:48 -0600

    Added a Product Repository.

---
+++ BookShoppingMall/BookShoppingMall.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookShoppingMall.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShoppingMall.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
