e597a315ccfade96746834ae53850d8dfbb3ae1a
goeunk <goeunkwak22@gmail.com>
2020-04-25 01:30:56 -0600

    Done with Cover Type functions(MVC).

---
+++ BookShoppingMall/BookShoppingMall.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using BookShoppingMall.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShoppingMall.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType coverType);
+    }
+}
